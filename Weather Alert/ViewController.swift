8ab80173db12a50ac42d9743965852cbbb59c864
Arnav Singhania <arnavsinghania98@gmail.com>
2018-10-22 23:47:15 -0700

    Connected components of storyboard to actual code file.

--- Weather Alert/ViewController.swift
+++ Weather Alert/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
